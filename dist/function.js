93b2cfc1221686d1e08338894434491da13d2969
maxx.diego <77diego.max@gmail.com>
2024-07-02 12:49:53 -0300

    @maxxdiego

--- dist/function.js
+++ dist/function.js
@@ -4,5 +4,17 @@ console.log("Como criar função");
 function getUser(name, lastname) {
     return `${name} ${lastname}`;
 }
-// Chamar a função
+// Chamar a função e imprimir o valor
 console.log(getUser("Diego", "Max"));
+// Criar a função para receber duas variáveis do tipo number e retornar um number
+function getSpeed(current, add) {
+    return current + add;
+}
+// Chamar a função e imprimir o valor
+console.log(getSpeed(9, 3));
+// Criar a função para receber duas variáveis do tipo number e retornar boolean
+function getSituation(current) {
+    return (current >= 7 ? true : false);
+}
+// Chamar a função e imprimir o valor
+console.log(getSituation(9));
--- src/function.ts
+++ src/function.ts
@@ -1,8 +1,22 @@
 console.log("Como criar função");
 
 // Criar a função para receber duas variáveis do tipo string e retornar a string
-function getUser(name: string, lastname: string){
+function getUser(name: string, lastname: string): string{
     return `${name} ${lastname}`
 }
-// Chamar a função
+// Chamar a função e imprimir o valor
 console.log(getUser("Diego", "Max"))
+
+// Criar a função para receber duas variáveis do tipo number e retornar um number
+function getSpeed(current: number, add: number): number{
+    return current + add
+}
+// Chamar a função e imprimir o valor
+console.log(getSpeed(9,3))
+
+// Criar a função para receber duas variáveis do tipo number e retornar boolean
+function getSituation(current: number): boolean {
+    return (current >=7 ? true : false)
+}
+// Chamar a função e imprimir o valor
+console.log(getSituation(9))
