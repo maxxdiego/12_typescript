927983aa56bcced7a284b3f6dd6eeaa4502423c8
maxx.diego <77diego.max@gmail.com>
2024-07-04 11:44:10 -0300

    @maxxdiego

---
+++ 02_typescript_react/src/pages/sobre.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Sobre() {
+  return (
+    <>
+      <Head>
+        <title>Sobre</title>
+        <meta name="description" content="Site modelo com Typescript e React" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h2>Sobre</h2>
+      </main>
+    </>
+  );
+}
