4a3aa347053dfea2849ce0d82c3aa5347feff34b
maxx.diego <77diego.max@gmail.com>
2024-07-06 13:43:34 -0300

    @maxxdiego

--- 02_typescript_react/layout/js/custom.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('#menu-btn').addEventListener('click', () => {
-    document.querySelector('#menu-site').classList.toggle("active")
-    document.querySelector('#menu-icon').classList.toggle("active")
-})
