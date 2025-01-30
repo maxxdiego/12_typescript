db1322d86bdfc6f8c7db6d72e058675bab6ab4b1
Diego Max <77diego.max@gmail.com>
2025-01-30 12:55:22 -0300

    @maxxdiego

---
+++ 03-typescript_react/01-setup-e-componentes-funcionais/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Greetings from "./components/Greetings";
+
+function App() {
+  return (
+    <>
+      <h1>React com TS</h1>
+      {/* Componente funcional com TS */}
+      <Greetings name="Diego" />
+    </>
+  );
+}
+
+export default App;
+
